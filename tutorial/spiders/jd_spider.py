6f05ebaef0be14148388fed3f73ac1d19033ed06
tenggyut <tengyutong0213@gmail.com>
2014-12-01 17:54:03 +0800

    fix bug

--- tutorial/spiders/jd_spider.py
+++ tutorial/spiders/jd_spider.py
@@ -13,8 +13,8 @@ class JdSpider(CrawlSpider):
     ]
 
     rules = (
-        Rule(LinkExtractor(allow=('http://list\.jd\.com/.*\.html', ))),
-        Rule(LinkExtractor(allow=('http://channel\.jd\.com/.*\.html', ))),
+        Rule(LinkExtractor(allow=('http://list\.jd\.com/.*\.html.*', ))),
+        Rule(LinkExtractor(allow=('http://channel\.jd\.com/.*\.html.*', ))),
         Rule(LinkExtractor(allow=('http://item\.jd\.com/.*\.html', )), callback='parse_item'),
     )
 
@@ -24,8 +24,8 @@ class JdSpider(CrawlSpider):
             if len(clog.xpath('text()').extract()) > 0:
                 catalog.append(clog.xpath('text()').extract()[0].encode('utf-8'))
             else:
-                self.log.info(response.url)
-        for img in response.xpath('//div[@class="spec-items"]/ul/li/img'):
+                self.log(response.url)
+        for img in response.xpath('//div[@class="spec-items"]/ul/li/img')[0:1]:
             pic = JdPicture()
             pic["image_urls"] = img.xpath('@src').extract()[0].encode('utf-8')
             pic["title"] = img.xpath('@alt').extract()[0].encode('utf-8')
